bfc14c9ab2cc93f41fb5cf371e908fd81a14520a
AndressaLp <andressarodrigueslopes157@gmail.com>
2025-03-25 00:00:18 -0300

    Criação das classes

---
+++ Domain/Servico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Servico
+    {
+        public int IdServico { get; set; }
+        public string NomeServico { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public bool StatusPagamento { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
---
+++ Domain/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NomeUsuario { get; set; }
+        public string NomeEmpresa { get; set; }
+        public string AreaProfissional { get; set; }
+        public string EmailUsuario { get; set; }
+        public string SenhaUsuario { get; set; }
+
+        public ICollection<Cliente> Clientes { get; set; }
+    }
+}
