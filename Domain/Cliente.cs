a86dbbe6fef5127df4b663d0a320cf9ec8435251
AndressaLp <andressarodrigueslopes157@gmail.com>
2025-05-03 11:34:03 -0300

    Repository - cliente

--- Domain/Cliente.cs
+++ Domain/Cliente.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
-        public int IdCliente { get; set; }
-        public string NomeCliente { get; set; }
-        public string Telefone { get; set; }
-        public string EmailCliente { get; set; }
-        public string EnderecoRua { get; set; }
-        public string Bairro { get; set; }
-        public string NumeroResidencia { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
+        public int ClienteId { get; set; }
+        public required string NomeCliente { get; set; }
+        public required string Telefone { get; set; }
+        public required string EmailCliente { get; set; }
+        public required string EnderecoRua { get; set; }
+        public required string Bairro { get; set; }
+        public required string NumeroResidencia { get; set; }
+        public required string Cidade { get; set; }
+        public required string Estado { get; set; }
         public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
-        public ICollection<Servico> Servicos { get; set; }
+        public required Usuario Usuario { get; set; }
+        //public required ICollection<Servico> Servicos { get; set; }
     }
 }
---
+++ Repository/ClienteRepository.cs
@@ -0,0 +1,64 @@
+﻿using Domain;
+using Infradb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClienteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClienteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Cliente> CriarCliente(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Cliente>> ObterClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        public async Task<Cliente?> ObterClienteId(int id)
+        {
+            return await _context.Clientes.FindAsync(id);
+        }
+
+        public async Task<Cliente?> AtualizarCliente(int id, Cliente clienteAtualizado)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if(cliente == null) return null;
+
+            cliente.Nome_cliente = clienteAtualizado.NomeCliente;
+            cliente.Telefone = clienteAtualizado.Telefone;
+            cliente.Email_cliente = clienteAtualizado.EmailCliente;
+            cliente.Endereco_rua = clienteAtualizado.EnderecoRua;
+            cliente.Numero_residencia = clienteAtualizado.NumeroResidencia;
+            cliente.Bairro = clienteAtualizado.Bairro;
+            cliente.Cidade = clienteAtualizado.Cidade;
+            cliente.Estado = clienteAtualizado.Estado;
+
+            await _context.SaveChangesAsync();
+            return cliente;
+        }
+
+        public async Task<bool> DeletarCliente(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if(cliente == null) return false;
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
