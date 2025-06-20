516257c11f067ea1268f84e3dc2f8f6bc1794524
Andressa Rodrigues Lopes <142153517+AndressaLp@users.noreply.github.com>
2025-06-20 13:44:12 -0300

    Update CarouselComponent.jsx

--- View/src/components/CarouselComponent.jsx
+++ View/src/components/CarouselComponent.jsx
@@ -9,10 +9,10 @@ function CarouselComponent(){
         <div className="h-full w-full flex items-center">
             <Swiper modules={[Navigation, Pagination, Autoplay]} navigation={true} pagination={{clickable: true}} autoplay={{delay: 10000}} spaceBetween={10}>
                 <SwiperSlide className="flex self-center">
-                    <img className="max-md:w-60 max-lg:w-72 w-90 m-auto pb-10 max-lg:px-10" src="../public/slide-1.png" alt="foto" />
+                    <img className="max-md:w-60 max-lg:w-72 w-90 m-auto pb-10 max-lg:px-10" src="/slide-1.png" alt="foto" />
                 </SwiperSlide>
                 <SwiperSlide className="flex self-center">
-                    <img className="max-md:w-60 max-lg:w-72 w-90 m-auto pb-10 max-lg:px-10" src="../public/slide-2.png" alt="" />
+                    <img className="max-md:w-60 max-lg:w-72 w-90 m-auto pb-10 max-lg:px-10" src="/slide-2.png" alt="" />
                 </SwiperSlide>
             </Swiper>
         </div>
